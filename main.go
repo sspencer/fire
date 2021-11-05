38f64a6c13b334d680c112eed5ad316b99a2b0fb
Steve Spencer <steve@bigfrog.net>
2021-11-05 15:15:50 -0700

    cleanup fetch

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"context"
 	"encoding/json"
 	firebase "firebase.google.com/go"
+	"firebase.google.com/go/db"
 	"flag"
 	"fmt"
 	"github.com/pkg/errors"
@@ -37,9 +38,8 @@ func main() {
 
 func fetch(obj string, keyPrint, prettyPrint, shallow bool) (string, error) {
 	ctx := context.Background()
-
 	conf := &firebase.Config{
-		DatabaseURL: env("FIRE_URL"), //"https://your-app.firebaseio.com",
+		DatabaseURL: env("FIRE_URL"),
 	}
 
 	opt := option.WithCredentialsFile(env("FIRE_ACCOUNT"))
@@ -58,58 +58,71 @@ func fetch(obj string, keyPrint, prettyPrint, shallow bool) (string, error) {
 	ref := client.NewRef(obj)
 
 	if keyPrint {
-		var data map[string]bool
-		if err := ref.GetShallow(ctx, &data); err != nil {
-			return "", errors.Wrap(err, "Error reading from database")
-		}
-
-		keys := make([]string, len(data))
-		i := 0
-		for k := range data {
-			keys[i] = k
-			i++
-		}
-
-		return strings.Join(keys, "\n"), nil
-
+		return keyFetch(ctx, ref)
+	} else if shallow {
+		return shallowFetch(ctx, ref, prettyPrint)
 	} else {
-		var data interface{}
-
-		if shallow {
-			if err := ref.GetShallow(ctx, &data); err != nil {
-				return "", errors.Wrap(err, "Error reading from database")
-			}
-		} else {
-			if err := ref.Get(ctx, &data); err != nil {
-				return "", errors.Wrap(err, "Error reading from database")
-			}
-		}
-
-		buf, err := json.Marshal(data)
-		if err != nil {
-			return "", errors.Wrap(err, "Error marshalling data")
-		}
-
-		if prettyPrint {
-			return jsonPrettyPrint(buf), nil
-		}
-		return string(buf), nil
+		return deepFetch(ctx, ref, prettyPrint)
 	}
 }
 
-func jsonPrettyPrint(buf []byte) string {
-	var out bytes.Buffer
-	err := json.Indent(&out, buf, "", "   ")
-	if err != nil {
-		return string(buf)
+func keyFetch(ctx context.Context, ref *db.Ref) (string, error) {
+	var data map[string]bool
+	if err := ref.GetShallow(ctx, &data); err != nil {
+		return "", errors.Wrap(err, "Error reading from database")
 	}
-	return out.String()
+
+	keys := make([]string, len(data))
+	i := 0
+	for k := range data {
+		keys[i] = k
+		i++
+	}
+
+	return strings.Join(keys, "\n"), nil
+}
+
+func shallowFetch(ctx context.Context, ref *db.Ref, prettyPrint bool) (string, error) {
+	var data interface{}
+	if err := ref.GetShallow(ctx, &data); err != nil {
+		return "", errors.Wrap(err, "Error reading from database")
+	}
+
+	return jsonPrettyPrint(data, prettyPrint)
+}
+
+func deepFetch(ctx context.Context, ref *db.Ref, prettyPrint bool) (string, error) {
+	var data interface{}
+	if err := ref.Get(ctx, &data); err != nil {
+		return "", errors.Wrap(err, "Error reading from database")
+	}
+
+	return jsonPrettyPrint(data, prettyPrint)
+}
+
+func jsonPrettyPrint(data interface{}, prettyPrint bool) (string, error) {
+	buf, err := json.Marshal(data)
+	if err != nil {
+		return "", errors.Wrap(err, "Error marshalling data")
+	}
+
+	if prettyPrint {
+		var out bytes.Buffer
+		err = json.Indent(&out, buf, "", "   ")
+		if err != nil {
+			return "", errors.Wrap(err, "Error formatting JSON")
+		}
+		return out.String(), nil
+	}
+
+	return string(buf), nil
 }
 
 func env(key string) string {
 	val, ok := os.LookupEnv(key)
 	if !ok {
 		fmt.Fprintf(os.Stderr, "Environment variable %s must be set\n", key)
+		os.Exit(1)
 	}
 	return val
 }
